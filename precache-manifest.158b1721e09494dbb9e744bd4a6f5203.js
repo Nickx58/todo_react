04eaf6035e92024023110b9149bfe3dcd447344c
nikhil8_sharma <nikhilx58@gmail.com>
2019-11-22 10:47:10 +0530

    Updates

---
+++ precache-manifest.158b1721e09494dbb9e744bd4a6f5203.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156b499b0221aeb30c6d0f6079902f39",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "537d520c03cee1ba4569",
+    "url": "/todo_react/static/css/main.d7311a52.chunk.css"
+  },
+  {
+    "revision": "a3c3489111dfd8b7d997",
+    "url": "/todo_react/static/js/2.51960002.chunk.js"
+  },
+  {
+    "revision": "537d520c03cee1ba4569",
+    "url": "/todo_react/static/js/main.5b82d767.chunk.js"
+  },
+  {
+    "revision": "b3eb1540de3d67818e4a",
+    "url": "/todo_react/static/js/runtime-main.d4699b91.js"
+  },
+  {
+    "revision": "40d5f23096695d4fbc2a29d8ca02c4c1",
+    "url": "/todo_react/static/media/bin.40d5f230.svg"
+  },
+  {
+    "revision": "c2625638b2a6db871bea29942fa946b7",
+    "url": "/todo_react/static/media/done.c2625638.svg"
+  },
+  {
+    "revision": "522a418bc396f1551eef594b611cab2f",
+    "url": "/todo_react/static/media/edit.522a418b.svg"
+  },
+  {
+    "revision": "8601f5bd9fb2d08026329c47364dde32",
+    "url": "/todo_react/static/media/plus.8601f5bd.svg"
+  },
+  {
+    "revision": "37acd06039bd0e35abea69a0f5b4449f",
+    "url": "/todo_react/static/media/task-archived.37acd060.svg"
+  },
+  {
+    "revision": "981ae3112abeb2825c924684168a72a0",
+    "url": "/todo_react/static/media/task-completed.981ae311.svg"
+  },
+  {
+    "revision": "9be522379615270d7989fbbb07bea912",
+    "url": "/todo_react/static/media/task-in-progress.9be52237.svg"
+  },
+  {
+    "revision": "2bfbf7339f2ebce75631bb77b06d8dd1",
+    "url": "/todo_react/static/media/task-pending.2bfbf733.svg"
+  },
+  {
+    "revision": "e7cffb9da4786f79de819029970890e0",
+    "url": "/todo_react/static/media/task-search.e7cffb9d.svg"
+  },
+  {
+    "revision": "c94aea67d5b5de12fe2e4c44e4c187c7",
+    "url": "/todo_react/static/media/www.c94aea67.svg"
+  }
+]);
